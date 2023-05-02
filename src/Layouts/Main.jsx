e12165783154d8be5d6159ef5595038753953231
Arman7052 <99arman7052@gmail.com>
2023-05-02 21:13:19 +0600

    first commit

---
+++ src/Layouts/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from '../Pages/Shared/Header/Header';
+import Footer from '../Pages/Shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+
+
+
+
+            
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
