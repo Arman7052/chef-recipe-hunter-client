0ad43b1c55507b003cd7223e1ef843f1a91c6d6d
Arman7052 <99arman7052@gmail.com>
2023-05-03 00:58:48 +0600

    Navbar done

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import Marquee from "react-fast-marquee";
 import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
 
 const Header = () => {
     return (
-        <div className=' text-center text-red-200 flex gap-4 justify-between lg:mx-10'>
-            <div className='py-10'>
+       <div>
+         <div className=' text-center bg-red-100 text-red-300 flex sm:justify-center lg:justify-between '>
+            <div className='lg:py-8 sm:py-5 lg:text-lg'>
                 <Marquee pauseOnHover={true} speed={30}>
                     Step into Taste Bengal and discover the vibrant tastes of Dhaka. Join us at our table!
 
                 </Marquee>
             </div>
-            <div className=' bg-red-50 rounded-full'> <Link to='/'><img className=' ' src="https://i.ibb.co/VqsdBf0/apple-touch-icon.png" alt="" />  </Link> </div>
-            <div className='py-10'>
+            <div className='bg-red-50 rounded-full '> <Link to='/'><img className=' ' src="https://i.ibb.co/VqsdBf0/apple-touch-icon.png" alt="" />  </Link> </div>
+            <div className='lg:py-8 sm:py-5  lg:text-lg'>
                 <Marquee pauseOnHover={true} speed={30} >
                     Step into Taste Bengal and discover the vibrant tastes of Dhaka. Join us at our table!
                 </Marquee>
             </div>
         </div>
+        <Navbar></Navbar>
+       </div>
+        
     );
 };
 
--- src/Pages/Shared/Header/Navbar.jsx
+++ src/Pages/Shared/Header/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
     return (
-        <div>
-            
+        <div className="navbar text-black ">
+            <div className="flex-1">
+                <Link to="/" className=" text-black font-bold text-2xl">
+                    <img className=' h-10 w-auto' src="https://i.ibb.co/B4S7mB9/Screenshot-2023-05-02-235921-removebg-preview.png" alt="" />
+                </Link>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/login'>Login</Link></li>
+                    <li><Link to='/signUp'>Sign Up</Link></li>
+                </ul>
+            </div>
         </div>
     );
 };
