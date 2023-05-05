c083e8d77ee3ad6bf9f418ac31991127b5e1e953
Arman7052 <99arman7052@gmail.com>
2023-05-05 09:35:26 +0600

    Active route added

--- src/Pages/Shared/Header/Navbar.jsx
+++ src/Pages/Shared/Header/Navbar.jsx
@@ -1,20 +1,21 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FaRegUserCircle } from 'react-icons/fa';
 import { AuthContext } from '../../../Providers/AuthProvider';
 
 const Navbar = () => {
-
     const { user, logOut } = useContext(AuthContext);
+    const location = useLocation();
 
     const handleLogout = () => {
         logOut()
             .then()
             .catch(error => console.log(error))
     }
+
     return (
         <div>
-            <div className="navbar text-lime-800 lg:font-bold px-10">
+            <div className="navbar text-lime-800 lg:font-normal px-10">
                 <div className="flex-1">
                     <Link to="/" className=" text-black font-bold text-2xl">
                         <img className='  h-12 lg:h-16 w-auto ' src="https://i.ibb.co/VqsdBf0/apple-touch-icon.png" alt="" />
@@ -22,8 +23,26 @@ const Navbar = () => {
                 </div>
                 <div className="flex-none">
                     <ul className="menu menu-horizontal text-sky-300 bg-transparent">
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/blog'>Blog</Link></li>
+                        <li>
+                            <Link
+                                to='/'
+                                className={`${
+                                    location.pathname === '/' ? 'font-bold' : ''
+                                }`}
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to='/blog'
+                                className={`${
+                                    location.pathname === '/blog' ? 'font-bold' : ''
+                                }`}
+                            >
+                                Blog
+                            </Link>
+                        </li>
                         <li>
                             {user &&
                                 <Link to='/login'>
@@ -45,3 +64,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
